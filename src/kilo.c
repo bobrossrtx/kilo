299a48e42b3b848e12045f7609d5a4a177cc53a3
bobrossrtx <bobrossrtx@gmail.com>
2021-04-24 20:12:32 +0100

    Added Version, Buffer, Dynamic Drawing/Refreshing

--- src/kilo.c
+++ src/kilo.c
@@ -4,15 +4,19 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
 /*** defines ***/
+
+#define KILO_VERSION "0.0.1"
 #define CTRL_KEY(key) ((key) & 0x1f)
 
 
 /*** data ***/
+
 struct editorConfig {
   int screenrows;
   int screencols;
@@ -24,6 +28,7 @@ struct editorConfig _editorConfig;
 
 
 /*** terminal ***/
+
 void die(const char *err) {
   write(STDOUT_FILENO, "\x1b[2J", 4);
   write(STDOUT_FILENO, "\x1b[H", 3);
@@ -82,14 +87,14 @@ int getCursorPosition(int *rows, int *cols) {
 
   if (buf[0] != '\x1b' || buf[1] != '[') return -1;
   if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
-  
+
   return 0;
 }
 
 int getWindowSize(int *rows, int *cols) {
   struct winsize ws;
 
-  if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
     if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
     return getCursorPosition(rows, cols);
   } else {
@@ -99,21 +104,70 @@ int getWindowSize(int *rows, int *cols) {
   }
 }
 
+
+/*** append buffer ***/
+
+struct abuf {
+  char *b;
+  int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len) {
+  char *new = realloc(ab->b, ab->len + len);
+
+  if (new == NULL) return;
+  memcpy(&new[ab->len], s, len);
+  ab->b = new;
+  ab->len += len;
+}
+
+void abFree(struct abuf *ab) {
+  free(ab->b);
+}
+
+
 /*** output ***/
 
-void editorDrawRows() {
+void editorDrawRows(struct abuf *ab) {
   for (int y = 0; y < _editorConfig.screenrows; y++) {
-    write(STDOUT_FILENO, "~\r\n", 3);
+    if (y == _editorConfig.screenrows / 3) {
+      char welcome[80];
+      int welcomelen = snprintf(welcome, sizeof(welcome), 
+        "Kilo editor -- version %s", KILO_VERSION);
+      if (welcomelen > _editorConfig.screencols) welcomelen = _editorConfig.screencols;
+      int padding = (_editorConfig.screencols - welcomelen) / 2;
+      if (padding) {
+        abAppend(ab, "~", 1);
+        padding--;
+      }
+      while (padding--) abAppend(ab, " ", 1);
+      abAppend(ab, welcome, welcomelen);
+    } else {
+      abAppend(ab, "~", 1);
+    }
+
+    abAppend(ab, "\x1b[K", 4);
+    if (y < _editorConfig.screenrows - 1) {
+      abAppend(ab, "\r\n", 2);
+    }
   }
 }
 
 void editorRefreshScreen() {
-  write(STDOUT_FILENO, "\x1b[2J", 4);
-  write(STDOUT_FILENO, "\x1b[H", 3);
+  struct abuf ab = ABUF_INIT;
 
-  editorDrawRows();
+  abAppend(&ab, "\x1b[?25l", 6);
+  abAppend(&ab, "\x1b[H", 3);
 
-  write(STDOUT_FILENO, "\x1b[H", 3);
+  editorDrawRows(&ab);
+
+  abAppend(&ab, "\x1b[H", 3);
+  abAppend(&ab, "\x1b[?25h", 6);
+
+  write(STDOUT_FILENO, ab.b, ab.len);
+  abFree(&ab);
 }
 
 
